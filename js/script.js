6a2ce941f783de0631c4b9d7444c4afdd84bbfc3
William Finocchiaro <89465648+WILLIAMFINOCCHIARO@users.noreply.github.com>
2021-11-19 17:56:18 +0100

    init

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Ricreiamo le card del nostro team aggiungendo al layout di base fornito, 
+// il nostro javascript in cui:
+// Creiamo il nostro array di oggetti che rappresentano ciascun membro del team. 
+// Ogni membro dovrà avere le informazioni necessarie per stampare 
+// la relativa card: Nome, Ruolo e Foto.
+// Prendendo come riferimento il layout di esempio 
+// presente nell'html, stampiamo tutte le card del nostro team.
+// BONUS: Utilizziamo poi gli input presenti nella pagina
+//  per permettere all'utente di aggiungere nuovi membri del team.
+// Consigli del giorno: Ragioniamo come sempre a step.
+// Prima la logica in italiano e poi traduciamo in codice. console.log() è nostro amico.
+// Quando un pezzo di codice funziona, chiediamoci 
+// se possiamo scomporlo in funzioni più piccole.
+// Buon lavoro e buon divertimento con gli oggetti! 
