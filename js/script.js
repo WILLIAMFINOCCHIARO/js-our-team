5ae1ae5624d67f105e0a6dc0550e817b1ca3cccd
William Finocchiaro <89465648+WILLIAMFINOCCHIARO@users.noreply.github.com>
2021-11-20 10:37:52 +0100

    complete

--- js/script.js
+++ js/script.js
@@ -1,54 +1,112 @@
-// Ricreiamo le card del nostro team aggiungendo al layout di base fornito, 
-// il nostro javascript in cui:
-// Creiamo il nostro array di oggetti che rappresentano ciascun membro del team. 
-// Ogni membro dovrà avere le informazioni necessarie per stampare 
-// la relativa card: Nome, Ruolo e Foto.
-// Prendendo come riferimento il layout di esempio 
-// presente nell'html, stampiamo tutte le card del nostro team.
-// BONUS: Utilizziamo poi gli input presenti nella pagina
-//  per permettere all'utente di aggiungere nuovi membri del team.
+// Ciao ragazzi, esercizio di oggi: Our Team
+// nome repo: js-our-team
+// Ricreiamo le card del nostro team aggiungendo al layout di base fornito, il nostro javascript in cui:
+// Creiamo il nostro array di oggetti che rappresentano ciascun membro del team. Ogni membro dovrà avere le informazioni necessarie per stampare la relativa card: Nome, Ruolo e Foto.
+// Prendendo come riferimento il layout di esempio presente nell'html, stampiamo tutte le card del nostro team.
+// BONUS: Utilizziamo poi gli input presenti nella pagina per permettere all'utente di aggiungere nuovi membri del team.
 // Consigli del giorno: Ragioniamo come sempre a step.
 // Prima la logica in italiano e poi traduciamo in codice. console.log() è nostro amico.
-// Quando un pezzo di codice funziona, chiediamoci 
-// se possiamo scomporlo in funzioni più piccole.
-// Buon lavoro e buon divertimento con gli oggetti! 
+// Quando un pezzo di codice funziona, chiediamoci se possiamo scomporlo in funzioni più piccole.
+// Buon lavoro e buon divertimento con gli oggetti! :laptop_parrot:
 
-// Creiamo un Array con gli oggetti da inserire
+// Creo l'array di oggetti. OK
+// Eseguo un ciclo in maniera tale da passare per ogni elemento dell'Array ed estrapolare gli oggetti
+// estrapolo gli elementi dell'oggetto.
+// creo le classi da creare all'interno del ciclo
+// (  attraverso il backtrick template o attraverso il create Element) in maniera tale da creare le classi per ogni oggetto dell'array
+// inserisci gli elementi estrapolati all'interno di una classe creata precedentemente
+// Seleziono il container in cui mettere le classi
+// inserisce le classi create nel container creato precedentemente
 
-const cardArray = [ 
-  {
-    "Nome": "Angela Carrol",
-    "Ruolo":"Walter Gordon",
-    "Foto": 'img/angela-caroll-chief-editor.jpg'
 
-  },
+// creo l'array di oggetti
 
-  {
-    "Nome": "Walter Gordon",
-    "Ruolo":"Office Manager",
-    "Foto": 'img/walter-gordon-office-manager'
+const teamCards = [
+   { 
+  "Nome" :"Angela Carroll",
+  "Ruolo" :"Chief Editor",
+  "Immagine":"img/angela-caroll-chief-editor.jpg"
+   },
 
-  },
+  { 
+    "Nome" :"Walter Gordon",
+    "Ruolo" :"Office Manager",
+    "Immagine":"img/walter-gordon-office-manager.jpg"
+    },
+    
+  { 
+    "Nome" :"Angela Lopez",
+    "Ruolo" :"Social Media Manager",
+    "Immagine":"img/angela-lopez-social-media-manager.jpg"
+    },
 
-  {
-    "Nome": "Angela Lopez",
-    "Ruolo":"Social Media Manager",
-    "Foto":'img/angela-lopez-social-media-manager'
+  { 
+    "Nome" :"Scott Estrada",
+    "Ruolo" :"Developer",
+    "Immagine":"img/scott-estrada-developer.jpg"
+    },
 
-  },
-
-  {
-    "Nome": "Scott Estrada",
-    "Ruolo":"Developer",
-    "Foto":'img/scott-estrada-developer'
-
-  },
-
-  {
-    "Nome": "Barbara Ramos",
-    "Ruolo":"Graphic Designer",
-    "Foto":'img/barbara-ramos-graphic-designer'
-
-  }
+  { 
+    "Nome" :"Barbara Ramos",
+    "Ruolo" :"Graphic Designer",
+    "Immagine":"img/barbara-ramos-graphic-designer.jpg"
+    }
 
 ];
+
+console.log(teamCards);
+
+
+// Eseguo un ciclo in maniera tale da passare per ogni elemento dell'Array ed estrapolare gli oggetti
+
+
+// Creo una variabile in maniera tale da creare le classi da inserire nel ciclo
+let items = "";
+
+
+for ( i = 0; i < teamCards.length; i++) {
+  // - creo delle variabili per estrapolare gli oggetti dell'Array
+  // estrapolo gli elementi di ogni oggetto attraverso il ciclo.
+  let Namecard = teamCards[i].Nome;
+  let Rolecard = teamCards[i].Ruolo;
+  let Imagecard = teamCards[i].Immagine;
+  console.log(Namecard, Rolecard,  Imagecard);
+  // creo le classi da creare all'interno del ciclo
+  // (  attraverso il backtrick template o attraverso 
+  //il create Element) in maniera tale da creare le classi per ogni oggetto dell'array
+  items += `
+  <div class="team-card">
+    <div class="card-image">
+      <img
+        src="${Imagecard}"
+        alt="${Namecard}"
+      </img>
+    </div>
+    <div class="card-text">
+      <h3>${Namecard}</h3>
+      <p>${Rolecard}</p>
+    </div>
+  </div>
+  `
+
+  console.log(items[i]);
+
+  
+};
+
+// Seleziono il container in cui mettere le classi
+
+const containerTeam = document.querySelector(".team-container");
+
+// inserisci le classi create, nel container creato precedentemente.
+containerTeam.innerHTML += items;
+
+
+// BONUS: Utilizziamo poi gli input presenti nella pagina per 
+//permettere all'utente di aggiungere nuovi membri del team.
+// Consigli del giorno: Ragioniamo come sempre a step.
+// Prima la logica in italiano e poi traduciamo in codice. console.log() è nostro amico.
+// Quando un pezzo di codice funziona, chiediamoci se possiamo scomporlo in funzioni più piccole.
+
+// Seleziono gli input presenti
+
